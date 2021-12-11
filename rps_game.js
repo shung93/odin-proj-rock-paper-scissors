bf5e0192ad2e4b0ed337544253270c2ce8a4d7c4
Sam H <shung93@uw.edu>
2021-12-11 13:30:11 +0000

    Added html and UI to play rps

--- rps_game.js
+++ rps_game.js
@@ -1,16 +1,5 @@
-/*
-function playRound(playerSelection, computerSelection) {
-    
-    let playerSelectionPrompt = prompt("Please choose your weapon")
-    let playerSelection = playerSelectionPrompt.toLowerCase()
-    
-    return playerSelection
-    // return who won
-}
-*/
-
-function playRound() {
-    let playerSelection = prompt("Please choose your weapon (either rock, paper, or scissors).");
+function playRound(playerRPS) {
+    let playerSelection = playerRPS;
     playerSelection = playerSelection.toLowerCase();
     const initComputerSelection = Math.floor(Math.random() * 3) + 1;
     let computerSelection;
@@ -36,49 +25,102 @@ function playRound() {
 function game_winner(playerSelection, computerSelection) {
 
     if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        console.log('Rock beats scissors')
+        console.log('Rock beats scissors.')
         return 1
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        console.log('Paper beats rock')
+        console.log('Paper beats rock.')
         return 1
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        console.log('Scissors beats paper')
+        console.log('Scissors beats paper.')
         return 1
+    }  else if (playerSelection == computerSelection) {
+        console.log('Tied...')
+        return null
     } else {
-        console.log('You lost to the computer. The computer chose ' + computerSelection + ' to beat you')
+        console.log('You lost the round. The computer chose ' + computerSelection + ' to beat you.')
         return 0
     }
 
 }
 
 function playGame() {
-    console.log("We're only playing 5 rounds - too bad.")
+    console.log("We're only playing 5 rounds - too bad.") // change this to score box
 
     let playerScore = 0
     let computerScore = 0
 
-    for (let i = 0; i < 5; i++) { 
-        let roundOutcome 
-        roundOutcome = playRound()
+    // for (let i = 0; i < 5; i++) { 
+    //     let roundOutcome 
+    //     roundOutcome = playRound(playerRPS = playerInput)
+    //     if (roundOutcome == 1) {
+    //         playerScore++ 
+    //     } else if (roundOutcome == 0) {
+    //         computerScore++
+    //     }
+    // }
 
-        if (roundOutcome == 1) {
-            playerScore++ 
-        } else if (roundOutcome == 0) {
-            computerScore++
-        }
-    }
+    let button = document.getElementsByClassName("button");
+    for (let i = 0; i < button.length; i++) {
+        button[i].addEventListener("mouseover", function(event) {
+            button[i].classList.add("hoverOut") // add hover to the CSS 
+            event.target.style.color = "gray" // change font to gray
+        });
 
-    if (playerScore > computerScore) {
-        console.log("Congrats..you won... You beat a PC at rock-paper-scissors...")
-    } else if (computerScore > playerScore) {
-        console.log("You lost...womp womp...")
-    } else {
-        console.log("You tied...how did you even get here...")
-    }
+        button[i].addEventListener("mouseout", function(event) {
+            button[i].classList.add("hoverIn"); // remove hover to the CSS 
 
-    console.log("You scored: " + playerScore + " points.")
-    console.log("The computer scored: " + computerScore + " points.")
+            setTimeout(function() {
+                button[i].classList.remove("hoverOut");
+                button[i].classList.remove("hoverIn");
+                event.target.style.color = "" // revert font color
+            }, 251);
+        });
+    };
+
+    for (let i = 0; i < button.length; i++) { 
+        let roundOutcome
+        button[i].addEventListener("click", function(event) {
+            roundOutcome = playRound(playerRPS = button[i]['id'])
+
+            if (roundOutcome == 1) {
+                playerScore++ 
+            } else if (roundOutcome == 0) {
+                computerScore++
+            }
+
+            document.getElementById("playerScore").innerHTML = "Player Score: " + playerScore;
+            document.getElementById("computerScore").innerHTML = "Computer Score: " + computerScore;
+
+            if (playerScore >= 5 || computerScore >= 5) {
+                if (playerScore > computerScore) {
+                    window.alert("Congrats..you won... You beat a PC at rock-paper-scissors...")
+                } else if (computerScore > playerScore) {
+                    window.alert("You lost...womp womp...")
+                } else {
+                    window.alert("You tied...how did you even get here...")
+                }
+                playerScore = 0;
+                computerScore = 0;
+                document.getElementById("playerScore").innerHTML = "";
+                document.getElementById("computerScore").innerHTML = "";
+            }
+
+        });
+    };
+
+    
+    
+
+    // console.log("You scored: " + playerScore + " points.")
+    // console.log("The computer scored: " + computerScore + " points.")
 
 }
 
-console.log(playGame())
+playGame()
+
+
+
+
+
+
+// console.log(playGame())
